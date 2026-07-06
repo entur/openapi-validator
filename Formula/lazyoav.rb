54a3b1cba3c2f5430bc7adb1251175507d735b14
jorgenosberg <86744858+jorgenosberg@users.noreply.github.com>
2026-07-06 08:47:04 +0000

    chore: update homebrew formula checksums

--- Formula/lazyoav.rb
+++ Formula/lazyoav.rb
@@ -9,19 +9,19 @@ class Lazyoav < Formula
   on_macos do
     on_intel do
       url "https://github.com/entur/openapi-validator/releases/download/tui-v#{version}/lazyoav-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "28bea8138196171a0aa588904a0def054fe09c1114eddecff935912938198382"
     end
 
     on_arm do
       url "https://github.com/entur/openapi-validator/releases/download/tui-v#{version}/lazyoav-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "dd3ed40be6facd21becb883c38e118ab6a4a18b52c6dccc15d26ade61f2cee86"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/entur/openapi-validator/releases/download/tui-v#{version}/lazyoav-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "eda6cad4518b5af00c926c4d24d1800c23258e5e1963806265d27f400234b161"
     end
   end
 
